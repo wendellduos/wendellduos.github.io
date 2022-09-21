cd940ef10176f614323330bde403e1b26c85663a
Wendel Duarte dos Santos <wendellduos@hotmail.com>
2022-09-21 20:43:11 -0300

    Fix
    Actual fix for missing text issue.

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-var ptBio = document.getElementById("bio");
-ptBio.innerHTML = "Life & Interests";
-
-var notesTitle = document.getElementById("notes");
-notesTitle.innerHTML = "Notes";
-
-var ptParag = document.getElementById("bio-text")
-ptParag.innerHTML = "As a child and a teenager, I saw myself as an unfitting member of society for my lack of capacity to interact with people around me, but this is no sob story. As time went by I was able to accept myself for who I am, which led me to try my best to achieve solitude.<br><br>Throughout this little journey of self-discovery, I've noticed how learning things gives me a sense of reason in life. Some of the many things I've learned over the years were music production, focusing on sound-design-oriented music genres, photography, all sorts of instruments but mainly the electric guitar, vector-based 2d animation, and more recently, programming.";
-
-var notesBody = document.getElementById("notesText");
-notesBody.innerHTML = "This website is my first independent dev project! Credits to Alura for efficiently teaching the basics of Front End development within ten days.";
-
-var otherText = document.getElementById("otherTxt");
-otherText.innerHTML = "Images/Videos:";
-
 function changeLang() {
   var buttonDisable = document.getElementById("changeLang").disabled = true;
   var buttonText = document.getElementById("changeLang");
+  var ptBio = document.getElementById("bio");
+  var notesTitle = document.getElementById("notes");
+
   buttonText.innerHTML = "Feito!";
   ptBio.innerHTML = "Vida & Interesses";
   notesTitle.innerHTML = "Notas";
@@ -26,10 +14,12 @@ function changeLang() {
 }
 
 function bioText() {
+  var ptParag = document.getElementById("bio-text");
   ptParag.innerHTML = "Quando criança e em minha adolescência, eu me via como um membro inadequado da sociedade por minha falta de capacidade de interagir com as pessoas ao meu redor, mas essa não é uma história triste. Com o passar do tempo, consegui me aceitar como sou, o que me levou a tentar o meu melhor para alcançar a solitude.<br><br>Ao longo dessa pequena jornada de autodescoberta, percebi que aprender coisas novas me dá um senso de razão na vida. Algumas das muitas coisas que aprendi ao longo dos anos foram produção musical, focando em gêneros musicais orientados ao design de som, fotografia, vários tipos de instrumentos, mas principalmente guitarra, animação 2D baseada em vetores e, mais recentemente, programação.";
 }
 
 function notesText() {
+  var notesBody = document.getElementById("notesText");
   notesBody.innerHTML = "Este site é meu primeiro projeto independente de desenvolvimento! Créditos à Alura por ensinar com eficiência os fundamentos do desenvolvimento Front End em dez dias.";
 }
 
@@ -39,5 +29,6 @@ function spotifyText() {
 }
 
 function otherProj() {
+  var otherText = document.getElementById("otherTxt");
   otherText.innerHTML = "Imagens/Vídeos:";
 }
